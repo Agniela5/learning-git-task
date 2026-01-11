7478595edf9cd181d4bfb8cf15f0a1fec7cbb763
Agnieszka Trela <atrela1987@gmail.com>
2026-01-11 21:48:40 +0100

    Print results

--- main.py
+++ main.py
@@ -7,4 +7,5 @@ for shop, product in shopping_list.items():
     product_capital = [p.title() for p in product]
     product_string = ", ".join(product_capital)
 
-    
+    print(f"Ide do {shop.title()}, kupuje tu nastepujace rzeczy: {product_string}")
+
